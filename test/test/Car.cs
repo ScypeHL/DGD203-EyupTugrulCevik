fc8339adce1f24b298df42c8b9f958e6fba76a64
ScypeHL <tugrulcevik1@gmail.com>
2024-10-17 01:42:54 +0300

    Simple experimental codes to observe objects

---
+++ test/test/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pro
+{
+    public class Car
+    {
+        public string name;
+        public int year;
+        public int hp;
+        public float fuel;
+        public float ef;
+
+        public Car(string name, int year, int hp, float fuel, float ef)
+        {
+            this.name = name;
+            this.year = year;
+            this.hp = hp;
+            this.fuel = fuel;
+            this.ef = ef;
+        }
+
+        void Condition()
+        {
+
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This car's name is " + name);
+            Console.WriteLine("This car was made in " + year);
+            Console.WriteLine("This car has " + hp + " horsepowers");
+            Console.WriteLine("This car has " + fuel + "L fuel tank");
+            Console.WriteLine("This car consumes " + ef + "L of fuel per 100 kilometers");
+            Console.WriteLine("This car can go for " + (fuel * 100 / ef) + " kilometers");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ test/test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace Pro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Honda s2000", 2002, 225, 60, 14);
+            Car car2 = new Car("Corvette", 2003, 345, 80, 23);
+
+            car1.Info();
+            car2.Info();
+        }
+    }
+}
