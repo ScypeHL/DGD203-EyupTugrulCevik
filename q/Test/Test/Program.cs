67a5b3e4ce3baca8845ac1724223cc6529c3910b
ScypeHL <tugrulcevik1@gmail.com>
2024-11-04 19:52:59 +0300

    test2

---
+++ q/Test/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pro
+{
+    class Program()
+    {
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.ChSelect();
+        }
+
+    }
+}
